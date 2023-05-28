e719b23ef211e4b91c29966c1c9f16d58a3ed4ff
Adam Spencer <aspencer636@gmail.com>
2023-05-28 18:31:07 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let btn = document.getElementById("button")
+btn.addEventListener("click", fetchData);
+
+async function fetchData () {
+
+    let have = document.getElementById('fromCurrency').value
+    let want = document.getElementById('toCurrency').value
+    let amount = document.getElementById('Old_Currency').value
+    
+    // Check if Amount is empty or less than 0
+   if (amount == "" || amount < 0.01){
+    document.getElementById('Output').innerHTML = "Please enter a valid positive number"
+    document.getElementById('Emoji1').innerHTML = "&#10060;"
+    document.getElementById('Emoji2').innerHTML = "&#10060;"
+    }
+
+    else{
+    const url = `https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have=${have}&want=${want}&amount=${amount}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '61f499a172msh92b2f75f99db9b8p103f59jsn0eabeb279269',
+            'X-RapidAPI-Host': 'currency-converter-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+            document.getElementById('Output').innerHTML = result.new_amount
+            document.getElementById('Emoji1').innerHTML = "&#128176;"
+            document.getElementById('Emoji2').innerHTML = "&#128176;"
+        }
+        catch (error) {
+        console.error(error);
+    }
+}
+}
